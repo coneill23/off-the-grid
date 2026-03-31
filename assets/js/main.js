e5fb89c9c7c9ad0693886f2b69bd3ff1ce042375
coneill23 <cmo180003@utdallas.edu>
2026-03-31 20:37:24 +0000

    Random Number added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,14 @@
 // main.js
 let rotation = 0;
 document.addEventListener("click", function() {
-    let min = 10;
-    let max = 40;
-    rotation += Math.floor(Math.random() * (max - min + 1)) + min;
+    //let min = 10;
+    //let max = 40;
+    rotation += randomNumber(1, 200);
+    //rotation += Math.floor(Math.random() * (max - min + 1)) + min;
     document.body.style.transform = `rotate(${rotation}deg)`;
+
 });
+
+function randomNumber(min, max) {
+return Math.floor(Math.random() * (max - min) +1) + min;
+}
