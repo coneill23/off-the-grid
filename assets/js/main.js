3c50a343b9cbec88955a0c4e075351e6d130774f
coneill23 <cmo180003@utdallas.edu>
2026-04-02 13:49:11 +0000

    adding transform

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,32 @@
 // main.js
-let rotation = 0;
-document.addEventListener("click", function() {
-    //let min = 10;
-    //let max = 40;
-    rotation += randomNumber(1, 200);
-    //rotation += Math.floor(Math.random() * (max - min + 1)) + min;
-    document.body.style.transform = `rotate(${rotation}deg)`;
 
+let grid = document.querySelectorAll(".grid-container");
+let divs = document.querySelectorAll(".grid-container div");
+
+function randomize() {
+    console.log(divs.length, "randomize!")
+
+    // affect the whole grid
+    grid[0].style.transform = `rotate(${Math.random() * 360}deg)`;
+
+    //just the grid divs
+    divs.forEach(function (div) {
+    let scale = randomNumber(.5, 1.2);
+    let translateX = randomNumber(0, 50);
+    let translateY = randomNumber(0, 20);
+    let rotate = randomNumber (0, 360);
+
+    div.style.transform = 
+    `scale (${scale})
+    translate(${translateX}%, ${translateY}%)
+    rotate (${rotate}deg)`;
 });
+}
+//randomize();
+document.addEventListener ("click", randomize);
+
 
 function randomNumber(min, max) {
-return Math.floor(Math.random() * (max - min) +1) + min;
+return Math.floor(Math.random() * (max - min +1)) + min;
 }
+
